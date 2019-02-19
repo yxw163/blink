67af31afdca326dbf39ec92ae792892f4cfe060e
yangxw <yangxw163@163.com>
2019-02-19 16:25:53 +0800

    create blink

---
+++ test.js
@@ -0,0 +1,17 @@
+   var fn = function (num) {
+     return new Promise((resolve, reject) => {
+       console.log('destion:' + num)
+       if (typeof num == 'number') {
+         resolve(num)
+       } else {
+         reject(num)
+       }
+     })
+   }
+
+   fn(2).then((num) => {
+     console.log('first:' + num)
+     return num + 1
+   }).then((num) => {
+     console.log('reject')
+   })
